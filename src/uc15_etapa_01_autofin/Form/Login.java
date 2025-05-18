282a2ac6f5080a738f842a28992f013099ab8eca
Carlos Lagos <carloseflagos@gmail.com>
2025-05-17 22:22:24 -0300

    Criação da classe conexao

--- src/uc15_etapa_01_autofin/Form/Login.java
+++ src/uc15_etapa_01_autofin/Form/Login.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package uc15_etapa_01_autofin;
+package uc15_etapa_01_autofin.Form;
 
 /**
  *
